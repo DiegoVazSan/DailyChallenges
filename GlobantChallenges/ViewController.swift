7fa59a4d03001358c528acb4e4d9973ad7eec14e
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-02-27 10:56:08 -0600

    Initial Commit

---
+++ GlobantChallenges/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlobantChallenges
+//
+//  Created by Diego Vazquez Sanchez on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemIndigo
+    }
+
+
+}
+
